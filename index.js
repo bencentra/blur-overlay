1f99f100ab6fb0a69bd1c5519f1054d92bb093f4
Ben Centra <bcentra@constantcontact.com>
2016-07-18 17:08:45 -0400

    add code linting

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 $(function () {
-
   var showing = false;
 
   $.widget('custom.blurOverlay', {
 
     // Defaults
     options: {
-      autoShow: false, // Set to true to show the overlay on init
-      content: '<h1>Hello, blur overlay!</h1>', // Default content to display
-      blurAmount: '12px' // Amount of pixels to blur by
+      // Set to true to show the overlay on init
+      autoShow: false,
+      // Default content to display
+      content: '<h1>Hello, blur overlay!</h1>',
+      // Amount of pixels to blur by
+      blurAmount: '12px'
     },
 
     // "Constructor"
-    _create: function() {
+    _create: function () {
       // Initialize the wrapper
       this.$wrapper = $('<div>').attr('class', 'blur-overlay-wrapper');
       this.$wrapper.css({
         '-webkit-filter': 'blur(0px)',
-        'filter': 'blur(0px)',
-        'transition': '-webkit-filter 600ms linear, filter 600ms linear'
+        filter: 'blur(0px)',
+        transition: '-webkit-filter 600ms linear, filter 600ms linear'
       });
       this.element.wrapAll(this.$wrapper);
       this.$wrapper = this.element.closest('.blur-overlay-wrapper').first();
@@ -30,8 +32,8 @@ $(function () {
       this.$overlay = $('<div>').attr('class', 'blur-overlay-overlay');
       this.$overlay.css({
         'z-index': 1000,
-        'opacity': 0,
-        'transition': 'opacity 600ms linear'
+        opacity: 0,
+        transition: 'opacity 600ms linear'
       });
       this.$overlay.append(this.$content);
       this.$overlay.appendTo('body');
@@ -57,15 +59,15 @@ $(function () {
         $('body').css('overflow', 'hidden');
         this.$wrapper.css({
           '-webkit-filter': 'blur(' + this.options.blurAmount + ')',
-          'filter': 'blur(' + this.options.blurAmount + ')'
+          filter: 'blur(' + this.options.blurAmount + ')'
         });
         this.$overlay.css({
-          'position': 'absolute',
-          'top': 0,
-          'bottom': 0,
-          'left': 0,
-          'right': 0,
-          'opacity': 1
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          opacity: 1
         });
         this.$content.show();
       }
@@ -79,13 +81,13 @@ $(function () {
         $('body').css('overflow', 'auto');
         this.$wrapper.css({
           '-webkit-filter': 'blur(0px)',
-          'filter': 'blur(0px)'
+          filter: 'blur(0px)'
         });
         this.$overlay.css({
-          'opacity': 0,
+          opacity: 0
         });
       }
-      showing = false
+      showing = false;
     },
 
     // Update the contents of the overlay
@@ -96,5 +98,4 @@ $(function () {
     }
 
   });
-
 });
